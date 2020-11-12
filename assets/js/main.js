02d6f25cdd50488888cca7cdbe2e83b9567c3de8
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-12 15:05:06 +0100

    Inserito il cognome
    Inserito il cognome da far inserire all'utente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
 // variabile nome
 var nome = prompt("Inserisci il tuo nome?");
 document.getElementById("nome").innerHTML = nome
+// variabile cognome
+var cognome = prompt("Inserisci il tuo cognome?");
+document.getElementById("cognome").innerHTML = cognome
