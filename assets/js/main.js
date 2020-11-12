f18e60ff838631f403407e7f241dbfa77859b160
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-12 15:11:46 +0100

    Inserito il colore preferito
    Inserito il colore preferito da far inserire all'utente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 // variabile nome
 var nome = prompt("Inserisci il tuo nome?");
-document.getElementById("nome").innerHTML = nome
+document.getElementById("nome").innerHTML = nome;
 // variabile cognome
 var cognome = prompt("Inserisci il tuo cognome?");
-document.getElementById("cognome").innerHTML = cognome
+document.getElementById("cognome").innerHTML = cognome;
+// variabile colore preferito
+var colorePreferito = prompt("Inserisci il tuo colore preferito?");
+document.getElementById("colorePreferito").innerHTML = colorePreferito;
